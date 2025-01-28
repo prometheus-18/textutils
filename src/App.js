7da0fad90c7ed325c2f4795bd7f97c2751d78043
Rahul Verma <mrrahulverma18@gmail.com>
2025-01-28 23:28:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,14 @@
 import './App.css';
+import Navbar from './componets/Navbar';
+import Textform from './componets/Textform';
 
 function App() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">TEXTUTILS</a>
-          <button 
-            className="navbar-toggler" 
-            type="button" 
-            data-bs-toggle="collapse" 
-            data-bs-target="#navbarSupportedContent" 
-            aria-controls="navbarSupportedContent" 
-            aria-expanded="false" 
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">About</a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input 
-                className="form-control me-2" 
-                type="search" 
-                placeholder="Search" 
-                aria-label="Search" 
-              />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
+      <Navbar title="TEXTUTILS" aboutText ="about Text Util"/>
+      <div className='container my-3'>
+      <Textform header="Enter the text to analysn below :"/>
+      </div>
     </>
   );
 }
